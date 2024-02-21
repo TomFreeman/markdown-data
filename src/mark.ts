fea0c9be0425da7fa0a96f9f9e9c6ff706eea6b7
Thomas Freeman <3540977+TomFreeman@users.noreply.github.com>
2024-02-21 10:38:42 +0000

    Fixes for inline stuff

--- src/mark.ts
+++ src/mark.ts
@@ -9,13 +9,30 @@ const parse_strategy: parsers = {
     "yaml": (text: string) => load(text)
 }
 
+function readWordOrQuote(walker: commonmark.NodeWalker): string {
+    var next = walker.next();
+
+    // If the next node is a quote, read the quoted thing
+    if (next?.node.literal === "\"" || next?.node.literal === "'") {
+        next = walker.next();
+        return next?.node.literal || "";
+    } else if (next?.node.type === "code") {
+        // If the next node is a code block, read the whole code block
+        return next?.node.literal || "";
+    }else {
+        // Read until the first space
+        return next?.node.literal?.split(" ")[0] || "";
+    }
+}
+
 export function readAllElements(body: string): elementCollection {
     const reader = new commonmark.Parser();
     const parsed = reader.parse(body);
     const walker = parsed.walker();
     const elements: elementCollection = {};
-    const match_inline = /<\? _data_:(\w+) \?>(\w+)/
-    const match_block = /_data_:(\w+)/
+    const html_block_regex = /<\? _data_:(\w+) \?>(\w+)/
+    const html_inline_regex = /<\? _data_:(\w+) \?>/
+    const code_block_regex = /_data_:(\w+)/
     let next = walker.next();
     if (next === null) {
         return elements;
@@ -24,19 +41,29 @@ export function readAllElements(body: string): elementCollection {
     do {
         if (next?.node.type === "html_block") {
             // Check if matches the custom pattern <? _data_:data1 ?>foo1
-            if (match_inline.test(next?.node.literal || "")) {
-                const matches = match_inline.exec(next?.node.literal || "");
+            if (html_block_regex.test(next?.node.literal || "")) {
+                const matches = html_block_regex.exec(next?.node.literal || "");
                 if (matches !== null) {
                     const key = matches[1];
                     const value = matches[2];
                     elements[key] = value;
                 }
             }
+        } else if (next?.node.type === "html_inline") {
+            // If current node matches the pattern <? _data_:data1 ?>
+            // then extract the value from the next node along
+            if (html_inline_regex.test(next?.node.literal || "")) {
+                const matches = html_inline_regex.exec(next?.node.literal || "");
+                if (matches !== null) {
+                    const key = matches[1];
+                    elements[key] = readWordOrQuote(walker);
+                }
+            }
         } else if (next?.node.type === "code_block") {
             // Check if the info string contains _data_:(\w+)
             const info = next?.node.info;
             if (info !== null) {
-                const matches = match_block.exec(info);
+                const matches = code_block_regex.exec(info);
                 if (matches !== null) {
                     const key = matches[1];
                     const value = next?.node.literal || "";
