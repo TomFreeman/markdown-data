8132967216611f7087180590da6e7c561cd0bb9f
Thomas Freeman <3540977+TomFreeman@users.noreply.github.com>
2024-02-21 09:33:49 +0000

    Debug logging

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,14 @@ async function run() {
     // Get the text of the issue or PR body
     const body = github.context.payload.issue?.body || github.context.payload.pull_request?.body;
 
+    core.debug(`Body:\n ${body}`);
+
     if (body != undefined) {
         core.info('Parsing markdown')
         // Parse the markdown for reference links as a first attempt at a syntax
         const elements = readAllElements(body)
+
+        core.debug(`Elements:\n ${JSON.stringify(elements)}`);
 
         if (Object.keys(elements).length === 0) {
             core.info('No data found');
